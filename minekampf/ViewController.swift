fc2d796c312068a86674de173ed8f9a16c0d7e1b
soyombo <soyombo0@naver.com>
2023-01-10 14:28:28 +0300

    Initial Commit

---
+++ minekampf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  minekampf
+//
+//  Created by Soyombo Mantaagiin on 10.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
